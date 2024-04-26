ea30e4e0f7a8a980ad5279f9e4f6f23e97fe1bbd
Rodolfo-Almeida-SH3 <139129269+RodollfoAlmeida@users.noreply.github.com>
2024-04-25 21:36:59 -0300

    Adicionar arquivos de projeto.

---
+++ MF-DEV-BACK-END-RODOLFO/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using MF_DEV_BACK_END_RODOLFO.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MF_DEV_BACK_END_RODOLFO.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MF-DEV-BACK-END-RODOLFO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MF_DEV_BACK_END_RODOLFO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
