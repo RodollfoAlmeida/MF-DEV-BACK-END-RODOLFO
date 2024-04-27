be02eba62e7bc040e078c97afaab5a6d2effdf48
Rodolfo-Almeida-SH3 <139129269+RodollfoAlmeida@users.noreply.github.com>
2024-04-27 14:02:29 -0300

    Criação Banco de dados e da tabela veiculo

---
+++ MF-DEV-BACK-END-RODOLFO/Models/Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MF_DEV_BACK_END_RODOLFO.Models
+
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o nome do Veiculo!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a Placa do veiculo!")]
+        public string Placa { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação do Veiculo!")]
+        public int AnoFabricacao { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o Ano do modelo do Veiculo!")]
+        public int AnoModelo { get; set; }
+    }
+}
